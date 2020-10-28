07ce8dcb09279a1ee94847ab803d54b4d567ae5c
jakedude72 <72116220+jakedude72@users.noreply.github.com>
2020-10-27 19:15:26 -0700

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -17,22 +17,26 @@ function setup(){
 
     
     ground = new Ground(600,height,1200,20)
+    ground2 = new Ground(50,height - 150,150,300)
 
-    box1 = new Box(700,420,70,70);
-    box2 = new Box(920,420,70,70);
-    pig1 = new Pig(810, 550);
-    log1 = new Log(810,560,300, PI/2);
+    box1 = new Box(900,420,70,70);
+    box2 = new Box(1120,420,70,70);
 
-    box3 = new Box(700,540,70,70);
-    box4 = new Box(920,540,70,70);
-    pig3 = new Pig(810, 420);
+    pig1 = new Pig(1010, 550);
+    log1 = new Log(1010,560,300, PI/2);
 
-    log3 =  new Log(810,400,300, PI/2);
-    log2 =  new Log(810,480,300, PI/2);
+    pig2 = new Pig(750, 550)
 
-    box5 = new Box(810,360,70,70);
-    log4 = new Log(760,320,150, PI/7);
-    log5 = new Log(870,320,150, -PI/7);
+    box3 = new Box(900,540,70,70);
+    box4 = new Box(1120,540,70,70);
+    pig3 = new Pig(1010, 420);
+
+    log3 =  new Log(1010,400,300, PI/2);
+    log2 =  new Log(1010,480,300, PI/2);
+
+    box5 = new Box(1010,360,70,70);
+    log4 = new Log(960,320,150, PI/7);
+    log5 = new Log(1070,320,150, -PI/7);
 
     bird = new Bird(100,100);
 
@@ -47,8 +51,10 @@ function draw(){
     box1.display();
     box2.display();
     ground.display();
+    ground2.display();
     pig1.display();
     log1.display();
+    pig2.display();
 
     box3.display();
     box4.display();
